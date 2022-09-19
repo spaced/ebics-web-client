16d4bf80446177f5d32cfaabaf5646f9d08705fc
honza <honza.toegel@gmail.com>
2022-09-19 23:14:18 +0200

    Fixed include reference of ConnectionStatus

--- ebics-web-ui/src/components/models/ebics-bank.ts
+++ ebics-web-ui/src/components/models/ebics-bank.ts
@@ -1,5 +1,5 @@
 import { EbicsVersion } from 'components/models/ebics-version';
-import { ConnectionStatus } from './connection-status';
+import { ConnectionStatus } from 'components/models/connection-status';
 
 export interface EbicsVersionSettings {
     version: EbicsVersion;
