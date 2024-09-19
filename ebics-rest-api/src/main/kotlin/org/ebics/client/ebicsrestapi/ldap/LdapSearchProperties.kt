a40d7ae9fba47dc06c406c9434da1186eeefb7e6
spaced <spaced.wombat@gmail.com>
2024-09-19 10:07:55 +0200

    better ldap defaulting and do not set ldap stuff in application yml

--- ebics-rest-api/src/main/kotlin/org/ebics/client/ebicsrestapi/ldap/LdapSearchProperties.kt
+++ ebics-rest-api/src/main/kotlin/org/ebics/client/ebicsrestapi/ldap/LdapSearchProperties.kt
@@ -6,13 +6,13 @@ import org.springframework.boot.context.properties.ConfigurationProperties
 
 @ConfigurationProperties(prefix = "spring.ldap.search")
 data class LdapSearchProperties (
-    val group: LdapSearchPattern,
-    val user: LdapSearchPattern,
+    val group: LdapSearchPattern = LdapSearchPattern("","member={0}"),
+    val user: LdapSearchPattern = LdapSearchPattern("","(uid={0})"),
     val mapping: Map<String,String>? // mapping of spring-role -> ldap-role
 )
 
 
 data class LdapSearchPattern(
     val base: String = "",
-    val filter: String
+    val filter: String = ""
 )
