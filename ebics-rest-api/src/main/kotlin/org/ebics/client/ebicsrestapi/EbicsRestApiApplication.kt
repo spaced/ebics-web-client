d4e5f707efdfd322d76cf519ed99471ffb236b3d
spaced <spaced.wombat@gmail.com>
2024-10-03 13:34:52 +0200

    properties resolver must be static

--- ebics-rest-api/src/main/kotlin/org/ebics/client/ebicsrestapi/EbicsRestApiApplication.kt
+++ ebics-rest-api/src/main/kotlin/org/ebics/client/ebicsrestapi/EbicsRestApiApplication.kt
@@ -34,34 +34,36 @@ class EbicsRestApiApplication : SpringBootServletInitializer() {
         return builder.sources(EbicsRestApiApplication::class.java)
     }
 
-    /**
-     * This allow external config file on path defined by system or env variable "EWC_CONFIG_HOME"
-     * set EWC_CONFIG_HOME=../path/to/configuration/
-     */
-    @Bean
-    fun propertyResolver(): PropertySourcesPlaceholderConfigurer {
-        return PropertySourcesPlaceholderConfigurer().apply {
-            try {
-                val resources = mutableListOf<Resource>(ClassPathResource("application.yml"))
-                val configHome = System.getProperty("EWC_CONFIG_HOME") ?: System.getenv("EWC_CONFIG_HOME")
-                if (configHome != null) {
-                    val configFileNameProp = "$configHome/config.properties"
-                    val configFileNameYaml = "$configHome/config.yaml"
-                    when {
-                        File(configFileNameProp).exists() -> resources.add(FileSystemResource(configFileNameProp))
-                        File(configFileNameYaml).exists() -> resources.add(FileSystemResource(configFileNameYaml))
-                        else -> throw IllegalArgumentException("Config file not found, please check your config folder $configHome, non of $configFileNameProp, $configFileNameYaml exist")
+    companion object {
+        /**
+         * This allow external config file on path defined by system or env variable "EWC_CONFIG_HOME"
+         * set EWC_CONFIG_HOME=../path/to/configuration/
+         */
+        @Bean
+        fun propertyResolver(): PropertySourcesPlaceholderConfigurer {
+            return PropertySourcesPlaceholderConfigurer().apply {
+                try {
+                    val resources = mutableListOf<Resource>(ClassPathResource("application.yml"))
+                    val configHome = System.getProperty("EWC_CONFIG_HOME") ?: System.getenv("EWC_CONFIG_HOME")
+                    if (configHome != null) {
+                        val configFileNameProp = "$configHome/config.properties"
+                        val configFileNameYaml = "$configHome/config.yaml"
+                        when {
+                            File(configFileNameProp).exists() -> resources.add(FileSystemResource(configFileNameProp))
+                            File(configFileNameYaml).exists() -> resources.add(FileSystemResource(configFileNameYaml))
+                            else -> throw IllegalArgumentException("Config file not found, please check your config folder $configHome, non of $configFileNameProp, $configFileNameYaml exist")
+                        }
+                        val logbackFile = "$configHome/logback.xml"
+                        if (!File(logbackFile).exists())
+                            throw IllegalArgumentException("$logbackFile doesn't exist, please provide valid logback file")
+                    } else {
+                        println("EWC_CONFIG_HOME is not set, all mandatory external properties must be set as system or environment variable")
                     }
-                    val logbackFile = "$configHome/logback.xml"
-                    if (!File(logbackFile).exists())
-                        throw IllegalArgumentException("$logbackFile doesn't exist, please provide valid logback file")
-                } else {
-                    println("EWC_CONFIG_HOME is not set, all mandatory external properties must be set as system or environment variable")
+                    setLocations(*resources.toTypedArray())
+                } catch (ex: Exception) {
+                    System.err.println("Failed to start application -> failed to configure property placeholder: $ex")
+                    throw ex
                 }
-                setLocations(*resources.toTypedArray())
-            } catch (ex: Exception) {
-                System.err.println("Failed to start application -> failed to configure property placeholder: $ex")
-                throw ex
             }
         }
     }
